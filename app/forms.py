822606d1b24f26e6d3af2b359257f69b7d230246
dev-gabrielchaves <dev.gchaves@gmail.com>
2023-11-08 12:38:35 -0300

    Finished user authentication

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,34 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, EmailField, PasswordField, SubmitField
-from wtforms.validators import Length, DataRequired, EqualTo
+from wtforms.validators import Length, DataRequired, EqualTo, ValidationError
 
 class LoginForm(FlaskForm):
     email = EmailField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
+from app.models import User
+
+# What I am doing below is just creating two functions that will be used for validations...
+# Like Length(), DataRequired(), etc...
+# The difference is that Length() and anothers are already built in...
+# But if they weren't we would need to do the same thing as below
+
+# Checks if the username is already registered in the db
+def validate_username(form, username):
+    user = User.query.filter_by(username=username.data).first()
+    if user:
+        raise ValidationError('This username was already choosen. Please type another one.')
+
+# Checks if the email is already registered in the db    
+def validate_email(form, email):
+    user = User.query.filter_by(email=email.data).first()
+    if user:
+        raise ValidationError('This email is already in use. Please type another one.')   
+
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[Length(min=5, max=20), DataRequired()])
-    email = EmailField('Email', validators=[DataRequired()])
+    username = StringField('Username', validators=[Length(min=5, max=20), DataRequired(), validate_username])
+    email = EmailField('Email', validators=[DataRequired(), validate_email])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[EqualTo('password'), DataRequired()])
     submit = SubmitField('Register')
--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,25 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, session
 from app import app, bcrypt, db
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 @app.route('/')
 def home():
-    return render_template('home.html', title='Home')
+    return render_template('home.html', title='Home', username=session.get('username'))
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            flash("You've been logged in successfully!")
+            session['id'] = user.id
+            session['email'] = user.email
+            session['username'] = user.username
+            return redirect(url_for('home'))
+        else:
+            flash("Couldn't find the user. Please check your email and password.")
     return render_template('login.html', title='Login', form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -23,5 +33,13 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash("You've been registered successfully!")
+        session['id'] = user.id
+        session['email'] = user.email
+        session['username'] = user.username
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
