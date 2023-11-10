1f459054e3ab1ac28310f0048c6def78026918ce
dev-gabrielchaves <dev.gchaves@gmail.com>
2023-11-10 09:03:55 -0300

    Quiz is working
    Finished to implement Quiz functionalities, I might change something later, but it is working for now

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, flash, session
+from flask import render_template, redirect, url_for, flash, session, request
 import requests
 from app import app, bcrypt, db
 from app.forms import LoginForm, RegistrationForm
@@ -10,7 +10,7 @@ def home():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if session.get('id'):
+    if session.get('username'):
         flash('You are already logged in!')
         return redirect(url_for('home'))
     else:
@@ -19,8 +19,6 @@ def login():
             user = User.query.filter_by(email=form.email.data).first()
             if user and bcrypt.check_password_hash(user.password, form.password.data):
                 flash("You've been logged in successfully!")
-                session['id'] = user.id
-                session['email'] = user.email
                 session['username'] = user.username
                 return redirect(url_for('home'))
             else:
@@ -29,7 +27,7 @@ def login():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if session.get('id'):
+    if session.get('username'):
         flash('You are already registered!')
         return redirect(url_for('home'))
     else:
@@ -42,8 +40,6 @@ def register():
             db.session.add(user)
             db.session.commit()
             flash("You've been registered successfully!")
-            session['id'] = user.id
-            session['email'] = user.email
             session['username'] = user.username
             return redirect(url_for('home'))
         return render_template('register.html', title='Register', form=form)
@@ -53,23 +49,44 @@ def logout():
     session.clear()
     return redirect(url_for('home'))
 
-@app.route('/quiz')
+# Function responsible for generating Quiz questions
+def generate_quiz():
+    # Getting questions from the TRIVIA API
+    response = requests.get('https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple')
+    if response.status_code == 200:
+        # Transforming response into a dictionary and getting the results, that are the questions
+        response_dict = response.json()
+        results = response_dict['results']
+        questions = []
+        # The reason of this for loop is just to get the needed information from the results
+        for result in results:
+            question = {}
+            question['question'] = result.get('question')
+            question['options'] = result.get('incorrect_answers')
+            question['options'].append(result.get('correct_answer'))
+            randomize_questions = set(question['options'])
+            list_of_randomized_questions = list(randomize_questions)
+            question['options'] = list_of_randomized_questions
+            question['correct_answer'] = result.get('correct_answer')
+            questions.append(question)
+        return questions
+
+@app.route('/quiz', methods=['GET', 'POST'])
 def quiz():
     if session.get('username'):
-        response = requests.get('https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple')
-        if response.status_code == 200:
-            response_dict = response.json()
-            results = response_dict['results']
-            questions = []
-            for result in results:
-                question = {}
-                question['question'] = result.get('question')
-                question['options'] = result.get('incorrect_answers')
-                question['options'].append(result.get('correct_answer'))
-                set_of_options = set(question['options'])
-                question['options'] = set_of_options
-                question['correct_answer'] = result.get('correct_answer')
-                questions.append(question)
-            return render_template('quiz.html', questions=questions)
+        if request.method == 'POST':
+            score = 0
+            for i, question in enumerate(session['quiz']):
+                correct_answer = question.get('correct_answer')
+                option_choosen = request.form.get(f'question-{i}')
+                if option_choosen == correct_answer:
+                    score += 1
+            flash(f"You've made {score} question(s) out of 10!")
+            return redirect(url_for('home'))
+        # Generates a different Quiz each time is called
+        elif request.method == 'GET':
+            # Saving the Quiz in the session to be used within the 'POST' request
+            session['quiz'] = generate_quiz() 
+            return render_template('quiz.html', questions=session['quiz'])
     flash('Before starting the Quiz, make sure to login or to subscribe!')
     return redirect(url_for('login'))
